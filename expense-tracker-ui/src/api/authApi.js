c348271350645697a1e57e3992ca281408310793
Lalith Kumar Jakanathan <lalithkumarjakanathan23cys@srishakthi.ac.in>
2026-06-01 21:07:07 +0530

    Update AUTH_URL to new endpoint

--- expense-tracker-ui/src/api/authApi.js
+++ expense-tracker-ui/src/api/authApi.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 //const AUTH_URL = "http://localhost:8081/api/auth";
-const AUTH_URL = "http://a02501fc06e4a4782a22fc3024f35f5e-523494114.us-east-1.elb.amazonaws.com:8081/api/auth";
+// const AUTH_URL = "http://a02501fc06e4a4782a22fc3024f35f5e-523494114.us-east-1.elb.amazonaws.com:8081/api/auth";
+const AUTH_URL = "http://ac04015e67ffc446fb096f094e79d5ed-203504087.us-east-1.elb.amazonaws.com:8081/api/auth";
 export const loginUser = (email, password) => {
   return axios.post(`${AUTH_URL}/login`, { email, password });
 };
