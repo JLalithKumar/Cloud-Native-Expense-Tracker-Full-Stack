fa5cd89ab703cf15f02ccc3db6f459fb50a01f72
Lalith Kumar Jakanathan <lalithkumarjakanathan23cys@srishakthi.ac.in>
2026-06-21 13:23:11 +0530

    Update authApi.js

--- expense-tracker-ui/src/api/authApi.js
+++ expense-tracker-ui/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const AUTH_URL = "http://localhost:8081/api/auth";
-const AUTH_URL =  "a2c3df5fd0627495588c7dbbb0dc89fa-1535481267.us-east-1.elb.amazonaws.com";
+const AUTH_URL =  "http://a2c3df5fd0627495588c7dbbb0dc89fa-1535481267.us-east-1.elb.amazonaws.com:8081/api/auth";
 // const AUTH_URL = "http://a02501fc06e4a4782a22fc3024f35f5e-523494114.us-east-1.elb.amazonaws.com:8081/api/auth";
 //const AUTH_URL = "http://ae8f0b020c8664fb9a5408daca066cea-1409214738.us-east-1.elb.amazonaws.com:8081/api/auth";
 //const AUTH_URL = "http://ac04015e67ffc446fb096f094e79d5ed-203504087.us-east-1.elb.amazonaws.com:8081/api/auth";
